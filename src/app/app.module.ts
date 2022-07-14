24213e78e9a059bb6da92b9a2bee67ec5ddad0ba
tothxmate <tothxmate@gmail.com>
2022-07-14 22:45:03 +0100

    Add angular editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AngularEditorModule } from '@kolkov/angular-editor';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,8 @@ import { ButtonComponent } from './shared/components/button.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/learn-grammar/learn-grammar.component.ts
+++ src/app/learn-grammar/learn-grammar.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'page-learn-grammar',
   template: `
-    <h1>LEARN GRAMMAR</h1>
+    <div class="main-wrapper">
+        <h1>Learn grammar</h1>
+        <app-button color="var(--secondary-color)" text="Edit"></app-button>
+        <angular-editor [placeholder]="'Enter text here...'"></angular-editor>
+    </div>
   `,
   styles: [`
+    .main-wrapper{
+      width: 68vw;
+    }
+    h1{
+      display:inline-block;
+    }
+    app-button{
+      float:right;
+    }
   `]
 })
 export class LearnGrammarComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
