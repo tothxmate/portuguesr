c04de7e70898070b69dc195195f5ec1d474a7ba7
tothxmate <tothxmate@gmail.com>
2022-07-14 19:39:15 +0100

    Add box shadow

--- src/app/navigation.component.ts
+++ src/app/navigation.component.ts
@@ -30,6 +30,7 @@ type MenuItem = {
       border-radius: var(--border-radius);
       flex: 1;
       padding: 1rem;
+      box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
     }
     .logo {
       width: 80%;
