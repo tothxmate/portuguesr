447e993dcdeb8e807879708c1c99f2a392939c8e
tothxmate <tothxmate@gmail.com>
2022-07-14 21:50:45 +0100

    Add reactive forms module and button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { WidgetComponent } from './shared/components/widget.component';
 import { SectionTitleComponent } from './dashboard/components/section-title.component';
 import { CreateLessonComponent } from './learn-words/components/create-lesson.component';
 import { LessonDetailsComponent } from './learn-words/components/lesson-details.component';
+import { ButtonComponent } from './shared/components/button.component';
 
 @NgModule({
   declarations: [
@@ -23,12 +25,14 @@ import { LessonDetailsComponent } from './learn-words/components/lesson-details.
     WidgetComponent,
     SectionTitleComponent,
     CreateLessonComponent,
-    LessonDetailsComponent
+    LessonDetailsComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/components/button.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  template: `
+    <button [style.backgroundColor]="color">{{ text }}</button>
+  `,
+  styles: [`
+    :host{
+        flex: 1;
+        padding: 1vw;
+    }
+    button {
+        border-radius: 8px;
+        border-width: 0;
+        color: white;
+        cursor: pointer;
+        display: inline-block;
+        font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial, sans-serif;
+        font-size: 14px;
+        font-weight: 500;
+        line-height: 20px;
+        list-style: none;
+        margin: 0;
+        padding: 10px 12px;
+        text-align: center;
+        transition: all 200ms;
+        vertical-align: baseline;
+        white-space: nowrap;
+        user-select: none;
+        -webkit-user-select: none;
+        touch-action: manipulation;
+    }
+  `]
+})
+export class ButtonComponent implements OnInit {
+  @Input() text: string | undefined;
+  @Input() color: string | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
