4d413bc67f05e567e5162ed4e28f4500d29a93d4
tothxmate <tothxmate@gmail.com>
2022-08-07 21:07:15 +0100

    Add learn grammar component

--- src/app/learn-grammar/learn-grammar.component.ts
+++ src/app/learn-grammar/learn-grammar.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Editor } from 'ngx-editor';
+import { LearnGrammarService } from './learn-grammar.service';
 
 @Component({
   selector: 'page-learn-grammar',
   template: `
     <div class="main-wrapper">
         <h1>Learn grammar</h1>
-        <app-button color="var(--secondary-color)" text="Edit"></app-button>
+        <app-button (click)="saveContent()" color="var(--secondary-color)" text="Save"></app-button>
+        <div class="NgxEditor__Wrapper" *ngIf="isContentLoaded">
+          <ngx-editor-menu [editor]="editor"> </ngx-editor-menu>
+          <ngx-editor
+            [editor]="editor"
+            [(ngModel)]="html"
+            [disabled]="false"
+            [placeholder]="'Type here...'"
+          ></ngx-editor>
+        </div>
+        <app-loading-indicator *ngIf="!isContentLoaded"></app-loading-indicator>
     </div>
   `,
   styles: [`
@@ -22,10 +34,28 @@ import { Component, OnInit } from '@angular/core';
   `]
 })
 export class LearnGrammarComponent implements OnInit {
-  constructor() { }
+  editor: Editor;
+  html: string = '';
+  isContentLoaded: boolean = false;
+  learnGrammarService: LearnGrammarService
+  constructor(learnGrammarService: LearnGrammarService) { 
+    this.editor = new Editor();
+    this.learnGrammarService = learnGrammarService
+    this.learnGrammarService.getGrammarServiceContent().subscribe((data:any)=>{
+      this.isContentLoaded = true
+      this.html = data.content
+    })
+  }
 
   ngOnInit(): void {
 
   }
 
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
+
+  saveContent():void {
+    this.learnGrammarService.postGrammarServiceContent(this.html)
+  }
 }
