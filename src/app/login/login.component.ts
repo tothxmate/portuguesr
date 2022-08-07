84430fcbd85b93c393e369a96628b2afabbd65fe
tothxmate <tothxmate@gmail.com>
2022-08-07 21:09:50 +0100

    Navigate router to learn-words on login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -85,7 +85,7 @@ export class LoginComponent implements OnInit{
 
   ngOnInit(): void {
     if(this.authService.isLoggedIn){
-      this.router.navigate(['dashboard'])
+      this.router.navigate(['learn-words'])
     }
   }
 
